fe4daab5339dd41bbee7178460d6dda58e6cbc7e
Terence Martin <github@nurdz.com>
2018-12-01 20:43:46 -0800

    Thread the test client for better UX response
    This splits the existing code out of the command it was originally in
    and into a thread so that it doesn't hang the UI while it's executing.

--- test_client.py
+++ test_client.py
@@ -1,6 +1,9 @@
 import sublime
 import sublime_plugin
 
+from threading import Thread
+
+import textwrap
 import socket
 import sys
 
@@ -9,12 +12,46 @@ import sys
 #     https://realpython.com/python-sockets/#echo-client
 #     https://docs.python.org/3.3/library/socket.html
 
-# Crude blocking socket example; should be threaded, non-blocking, check
-# errors better, etc.
-#
-# If our protocol is binary, use struct.pack() to prepare data.
-class SocketTestCommand(sublime_plugin.ApplicationCommand):
-    def run(self, host, port):
+
+## ---------------------------------------------------------------------------
+
+
+def log(msg, *args, dialog=False, error=False, **kwargs):
+    """
+    Generate a message to the console and optionally as either a message or
+    error dialog. The message will be formatted and dedented before being
+    displayed, and will be prefixed with its origin.
+    """
+    msg = textwrap.dedent(msg.format(*args, **kwargs)).strip()
+
+    if error:
+        print("remote_build:")
+        return sublime.error_message(msg)
+
+    for line in msg.splitlines():
+        print("remote_build: {msg}".format(msg=line))
+
+    if dialog:
+        sublime.message_dialog(msg)
+
+
+### ---------------------------------------------------------------------------
+
+
+class NetworkThread(Thread):
+    """
+    Spawns a background thread that will perform our socket IO.
+    """
+    def __init__(self, host, port):
+        super().__init__()
+        log("Creating NetworkThread{0}", self)
+        self.host = host
+        self.port = port
+
+    def __del__(self):
+        log("Destroing NetworkThread{0}", self)
+
+    def run(self):
         try:
             # Create a socket
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
@@ -27,7 +64,7 @@ class SocketTestCommand(sublime_plugin.ApplicationCommand):
                 # Connect; anecdotally instead of a host this should be a
                 # resolved IP address -- the first resolved address is used,
                 # which may  be either IPv4 or IPv6.
-                sock.connect((host, port))
+                sock.connect((self.host, self.port))
 
                 # Sends may be short; send returns the number of bytes sent (I
                 # assume buffered at the socket level). Apparently there is a
@@ -48,7 +85,26 @@ class SocketTestCommand(sublime_plugin.ApplicationCommand):
                 data = data[:pos] if pos > 0 else data
 
                 # Display it.
-                sublime.message_dialog('Received:\n' + data)
+                sublime.set_timeout(lambda:
+                    log("Received\n{0}", data, dialog=True))
 
         except Exception as error:
-            sublime.error_message("Socket Error:\n%s" % error)
+            err = error
+            sublime.set_timeout(lambda:
+                log("Socket Error:\n{0}", err, error=True))
+
+
+### ---------------------------------------------------------------------------
+
+
+# Crude blocking socket example; should be threaded, non-blocking, check
+# errors better, etc.
+#
+# If our protocol is binary, use struct.pack() to prepare data.
+class SocketTestCommand(sublime_plugin.ApplicationCommand):
+    def run(self, host, port):
+        NetworkThread(host, port).start()
+
+
+### ---------------------------------------------------------------------------
+
