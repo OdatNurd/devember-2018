a0ea4355975804670abf680ce998b9138bac16f7
Terence Martin <github@nurdz.com>
2018-12-07 22:50:37 -0800

    Include some simple byte tests
    Using the new library, this does some tests to ensure that we understand
    how to work with byte arrays for the upcoming changes.

--- remote_build_server/Main.cs
+++ remote_build_server/Main.cs
@@ -3,6 +3,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
+using MiscUtil.Conversion;
 
 // The state object for reading client data.
 public class StateObject
@@ -216,7 +217,69 @@ public class AsynchronousSocketListener
     // Our entry point; this just starts us listening.
     public static int Main(String[] args)
     {
-        StartListening();
+        // StartListening();
+
+        // A 32 bit test value, and what it appears as when you encode it as
+        // a big endian value; value here taken from using struct.pack() in
+        // Python.
+        UInt32 sample = 1234567890;
+        byte[] test = {73, 150, 2, 210};
+
+        // Our converters; we really only want the big endian one.
+        var lec = new LittleEndianBitConverter();
+        var bec = new BigEndianBitConverter();
+
+        Console.WriteLine("Input: {0}", sample);
+        Console.WriteLine("       {0}", test.ToByteString());
+        Console.WriteLine("");
+
+        // This shows that the internal BitConverter is little endian because
+        // the local machine is little endian, but using MiscUtils we can
+        // force the endian.
+        Console.WriteLine("======= Decoding the byte array ===============");
+        Console.WriteLine("BitConverter => {0}", BitConverter.ToUInt32(test, 0));
+        Console.WriteLine("LittleEndianBitConverter => {0}", lec.ToUInt32(test, 0));
+        Console.WriteLine("BigEndianBitConverter => {0}", bec.ToUInt32(test, 0));
+
+        // This tests going the other way; given our input value, convert it to
+        // a byte array and display it using the extension method. Again the
+        // internal converter is always little endian.
+        Console.WriteLine("");
+        Console.WriteLine("======= Encoding the byte array ===============");
+        Console.WriteLine("BitConverter => {0}", BitConverter.GetBytes(sample).ToByteString());
+        Console.WriteLine("LittleEndianBitConverter => {0}", lec.GetBytes(sample).ToByteString());
+        Console.WriteLine("BigEndianBitConverter => {0}", bec.GetBytes(sample).ToByteString());
+
+        // Allocate a byte array and put our data into it
+        byte[] myArray = new byte[8];
+
+        // Just as a test, this is the method for quickly copying data from one
+        // array to another, should that be needed.
+        Console.WriteLine("");
+        Console.WriteLine("======= Encoding the byte array ===============");
+        Console.WriteLine("Original: {0}", myArray.ToByteString());
+
+        Buffer.BlockCopy(test, 0, myArray, 2, 4);
+        Console.WriteLine("Copied: {0}", myArray.ToByteString());
+        Console.WriteLine("BigEndianBitConverter => {0}", bec.ToUInt32(myArray, 2));
+
+        Console.WriteLine("Done");
         return 0;
+    }
+}
+
+public static class Extensions
+{
+    public static string ToByteString(this byte[] byteArray)
+    {
+        var sb = new StringBuilder("(");
+        for (var i = 0 ; i < byteArray.Length ; i++)
+        {
+            sb.Append(byteArray[i]);
+            if (i < byteArray.Length - 1)
+                sb.Append(", ");
+        }
+        sb.Append(")");
+        return sb.ToString();
     }
 }
