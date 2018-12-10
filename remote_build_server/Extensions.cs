f74f36b747eebbd649614ac1c88fa202702aa521
Terence Martin <github@nurdz.com>
2018-12-09 20:30:25 -0800

    Shift the extension code into its own class file
    This makes the top level code a little nicer to look at, and we may have
    more extension methods coming in the near future.

---
+++ remote_build_server/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+// using MiscUtil.Conversion;
+
+public static class Extensions
+{
+    public static string ToByteString(this byte[] byteArray)
+    {
+        var sb = new StringBuilder("(");
+        for (var i = 0 ; i < byteArray.Length ; i++)
+        {
+            sb.Append(byteArray[i]);
+            if (i < byteArray.Length - 1)
+                sb.Append(", ");
+        }
+        sb.Append(")");
+        return sb.ToString();
+    }
+}
--- remote_build_server/Main.cs
+++ remote_build_server/Main.cs
@@ -318,19 +318,3 @@ public class AsynchronousSocketListener
         return 0;
     }
 }
-
-public static class Extensions
-{
-    public static string ToByteString(this byte[] byteArray)
-    {
-        var sb = new StringBuilder("(");
-        for (var i = 0 ; i < byteArray.Length ; i++)
-        {
-            sb.Append(byteArray[i]);
-            if (i < byteArray.Length - 1)
-                sb.Append(", ");
-        }
-        sb.Append(")");
-        return sb.ToString();
-    }
-}
