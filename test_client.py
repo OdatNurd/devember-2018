127333b14c8f2f2e1d1a8e025ba21f938852b05a
Terence Martin <github@nurdz.com>
2018-12-10 22:17:43 -0800

    Re-implement our test command using new code
    This re-implements the code that was in the original day 0 code that
    sends a text message and pops up a dialog to display it to us so that
    it works with our new network code.

--- test_client.py
+++ test_client.py
@@ -8,34 +8,10 @@ import textwrap
 import socket
 import sys
 
-# related:
-#     https://docs.python.org/3.3/howto/sockets.html
-#     https://realpython.com/python-sockets/#echo-client
-#     https://docs.python.org/3.3/library/socket.html
+from .messages import ProtocolMessage, IntroductionMessage
+from .messages import MessageMessage, ErrorMessage
 
-
-## ---------------------------------------------------------------------------
-
-
-_jobQueue = None
-_jobThread = None
-
-
-## ---------------------------------------------------------------------------
-
-
-def plugin_loaded():
-    global _jobQueue, _jobThread
-
-    _jobQueue = Queue()
-    _jobThread = NetworkThread()
-    _jobThread.start()
-
-
-def plugin_unloaded():
-    if _jobThread is not None:
-        _jobQueue.put((None, None, None))
-        _jobThread.join(0.25)
+from .network import mgr
 
 
 ## ---------------------------------------------------------------------------
@@ -63,117 +39,12 @@ def log(msg, *args, dialog=False, error=False, **kwargs):
 ### ---------------------------------------------------------------------------
 
 
-class NetworkThread(Thread):
-    """
-    Spawns a background thread that will perform our socket IO.
-    """
-    def __init__(self):
-        super().__init__()
-        log("Creating NetworkThread{0}", self)
-
-    def __del__(self):
-        log("Destroing NetworkThread{0}", self)
-
-    def run(self):
-        running = True
-        while running:
-            host, port, msg = _jobQueue.get()
-            if host is None:
-                running = False
-            else:
-                self.run_query(host, port, msg)
-
-    def run_query(self, host, port, msg):
-        msg =  msg + "<EOF>"
-
-        try:
-            # Create a socket
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-                # Make the socket non-blocking; requires calls to select, so
-                # lets skip that for now. Could maybe also use settimeout()
-                # in combination with a select() to verify that an operation
-                # may work and then use a blocking read.
-                # sock.setblocking(0)
-
-                # Connect; anecdotally instead of a host this should be a
-                # resolved IP address -- the first resolved address is used,
-                # which may be either IPv4 or IPv6.
-                sock.connect((host, port))
-
-                # Sends may be short; send returns the number of bytes sent (I
-                # assume buffered at the socket level). Apparently there is a
-                # sendall(), but from looking at it we probably don't want it
-                # because if a send is interrupted by a signal it errors out
-                # and you can't tell how much was sent.
-                #
-                # Sends need to be bytes.
-                sock.send(msg.encode('utf-8'))
-
-                # Blocking receive of the data;
-                #
-                # Read data is bytes
-                data = sock.recv(1024).decode('utf-8')
-
-                # If we got a whole read, throw the trailer off.
-                pos = data.index("<EOF>")
-                data = data[:pos] if pos > 0 else data
-
-                # Display it.
-                sublime.set_timeout(lambda:
-                    log("Received\n{0}", data, dialog=True))
-
-        except Exception as error:
-            err = error
-            sublime.set_timeout(lambda:
-                log("Socket Error:\n{0}", err, error=True))
-
-
-class ProtocolMessage():
-    """
-    The base class for all protocol messages sent to and received from the
-    remote end. The constructor can take any number of keyword arguments, but
-    the _data argument is special and tells the constructor that it should
-    construct based on the bytes in the data instead.
-    """
-    def __init__(self, _data=None, **kwargs):
-        if _data:
-            self.decode(_data)
-
-    def msg_id(self):
-        """
-        The unique protocol message id for messages of this type. This is used
-        to associated protocol messages classes with the id values they
-        represent.
-        """
-        pass
-
-    def encode(self):
-        """
-        Given the state of this object, return back a protocol formatted bytes
-        object that can be used to reconstruct the object later. The first
-        bytes in the encoded data must be the msg_id() of this message type
-        so that the received knows what message type it is.
-        """
-        pass
-
-    def decode(self, data):
-        """
-        Given a piece of data, set up this object by decoding that data back
-        into the appropriate parts. The length of the data can be queried or
-        verified to ensure that things work as we expect.
-        """
-        pass
-
-
-### ---------------------------------------------------------------------------
-
-
-# Crude blocking socket example; should be threaded, non-blocking, check
-# errors better, etc.
-#
-# If our protocol is binary, use struct.pack() to prepare data.
-class SocketTestCommand(sublime_plugin.ApplicationCommand):
+class SocketTestCommand(sublime_plugin.WindowCommand):
     last_msg = "Hello, World!"
+
+    def __init__(self, window):
+        super().__init__(window)
+        self.connection = None
 
     def run(self, host, port):
         sublime.active_window().show_input_panel(
@@ -183,8 +54,16 @@ class SocketTestCommand(sublime_plugin.ApplicationCommand):
 
     def test(self, host, port, msg):
         self.last_msg = msg
-        _jobQueue.put((host, port, msg))
+        if self.connection is None:
+            self.connection = mgr.connect("dart", 50000)
+
+        self.connection.send(MessageMessage(msg))
+        sublime.set_timeout(lambda: self.result(), 500)
+
+    def result(self):
+        msg = self.connection.receive()
+        if msg is not None:
+            log("Received:\n{0}", msg.msg, dialog=True)
 
 
 ### ---------------------------------------------------------------------------
-
