0d633cc81776268faeb71314661ec96378d661a7
Terence Martin <github@nurdz.com>
2018-12-16 21:18:25 -0800

    Set up test framework for file set diffing
    This sets up our test command to have a known set of files that we can
    "diff" against in order to see what sort of changes we need to transmit
    to the other end, if any, in order to bring them in sync with us.

--- file_gather.py
+++ file_gather.py
@@ -10,32 +10,55 @@ import os
 from os.path import dirname, basename
 
 
-# From a build system
-spec1 = [{
-    "path": "/home/tmartin/local/src/devember-2018/",
-
-    "folder_include_patterns": [],
-    "file_include_patterns": [],
-
-    "folder_exclude_patterns": [],
-    "file_exclude_patterns": [],
-}]
-
-# From a build system
-spec2 = [{
-    "folder_exclude_patterns":
-    [
-        "obj",
-        "bin",
-        "__pycache__"
-    ],
-    "path": "/home/tmartin/local/src/devember-2018/"
-}]
-
-spec3 = [{
-}]
-
-spec4 = []
+test_folder = {
+  "/home/tmartin/local/src/devember-2018/remote_build_server": {
+    "BuildClient.cs": {
+      "last_modified": 1544504473.2212374,
+      "name": "BuildClient.cs",
+      "sha1": "201b43d4ddb4dc395c09b10e83305227b643db50"
+    },
+    "Extensions.cs": {
+      "last_modified": 1544416126.3528106,
+      "name": "Extensions.cs",
+      "sha1": "3e137214ffd6dfdd52b8ff3bb46e9fdab96fab7c"
+    },
+    "Main.cs": {
+      "last_modified": 1544502070.7733796,
+      "name": "Main.cs",
+      "sha1": "e596ccd55c6b253f14f09a7b68826e22fbb59f8e"
+    },
+    "messages/Error.cs": {
+      "last_modified": 1544506203.8349078,
+      "name": "messages/Error.cs",
+      "sha1": "cfbdb4439a29e4cb679b17982c6f5b78c23e3642"
+    },
+    "messages/Introduction.cs": {
+      "last_modified": 1544506197.8239672,
+      "name": "messages/Introduction.cs",
+      "sha1": "9f14770213767254dd30ee038299d97a712d872f"
+    },
+    "messages/Message.cs": {
+      "last_modified": 1544506089.9700332,
+      "name": "messages/Message.cs",
+      "sha1": "8958b712e1a6baca2ae74f53aa8e808a0f9a15ce"
+    },
+    "messages/PartialMessage.cs": {
+      "last_modified": 1544502110.762907,
+      "name": "messages/PartialMessage.cs",
+      "sha1": "78d85db5de04f841bf6a3832429add6727a64d24"
+    },
+    "messages/ProtocolMessageFactory.cs": {
+      "last_modified": 1544504869.4056778,
+      "name": "messages/ProtocolMessageFactory.cs",
+      "sha1": "8b2b7f3fa05ef35857e7eee56cd9c1f7a9ccdbad"
+    },
+    "remote_build_server.csproj": {
+      "last_modified": 1544225083.3561223,
+      "name": "remote_build_server.csproj",
+      "sha1": "e61459af3f70865b126f5d828adac52aef5a2968"
+    }
+  }
+}
 
 # TODO: this requires some case sensitive path checks
 def _keep(filename, includes, excludes):
@@ -226,6 +249,15 @@ def _find_project_files(window, folders=None, hash_files=True):
     return _coalesce_folders(files)
 
 
+def calculate_fileset_deltas(us, them):
+    """
+    Given two fileset dictionarys, one representing our files and one
+    representing "their" files, this returns back a dictionary that indicates
+    what files need to be added, removed or updated in order to make their
+    files match ours.
+    """
+    return {}
+
 # Any window with folders open always:
 #   1) responds to window.folders() with a list of folders (absolute)
 #   2) responds to window.project_data() with at least the "folders" key
@@ -234,27 +266,15 @@ def _find_project_files(window, folders=None, hash_files=True):
 #   1) window.project_file_name()
 class FileGatherCommand(sublime_plugin.WindowCommand):
     def run(self):
-        files = _find_project_files(self.window, folders=None)
-            #     {
-            #         "path": "/home/tmartin/local/src/devember-2018/",
-            #         "folder_exclude_patterns":
-            #         [
-            #             "obj",
-            #             "bin",
-            #             "__pycache__"
-            #         ],
-            #     },
-            #     {
-            #         "path": "/home/tmartin/local/src/devember-2017/",
-            #     }
-            # ])
-        # pprint(files)
-        print(json.dumps(files, indent=2, sort_keys=True))
-
-        # dirs = ['.git', 'remote_build_server', 'net_test', 'enum']
-        # dirs[:] = _prune_folders(dirs, [], [".git", ".svn"])
-        # pprint(dirs)
-
-        # filename = "sample.txt"
-        # print(filename, _keep(filename, ["*.txt"], []))
-
+        files = _find_project_files(self.window, folders=[
+                {
+                    "path": "/home/tmartin/local/src/devember-2018/remote_build_server",
+                    "folder_exclude_patterns":
+                    [
+                        "obj",
+                        "bin",
+                    ],
+                },
+            ])
+        diffed = calculate_fileset_deltas(files, test_folder)
+        print(json.dumps(diffed, indent=2, sort_keys=True))
