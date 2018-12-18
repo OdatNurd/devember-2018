6dc509cf39f170be24f235de27e0755abbee03af
Terence Martin <github@nurdz.com>
2018-12-17 22:00:08 -0800

    Enhance the C# Introduction message
    Similar to the recent change to the Python code, this enhances the
    IntroductionMessage to have the new fields the same as that version so
    that they can inter-operate.
    
    This uses the new fixed length string functionality.

--- remote_build_server/messages/Introduction.cs
+++ remote_build_server/messages/Introduction.cs
@@ -10,32 +10,51 @@ public class IntroductionMessage : IProtocolMessage
     /// </summary>
     public byte ProtocolVersion { get ; private set; } = 1;
 
-    public IntroductionMessage()
-    {
+    public string User { get ; private set; }
+    public string Password { get; private set; }
+    public string Hostname { get; private set; }
+    public string Platform { get; private set; }
 
+    public IntroductionMessage(string user, string password, string hostname, string platform)
+    {
+        User = user;
+        Password = password;
+        Hostname = hostname;
+        Platform = platform;
     }
 
     public IntroductionMessage(byte[] data)
     {
-        if (data.Length != 3)
+        if (data.Length != 2 + 1 + 64 + 64 + 64 + 8)
             throw new ArgumentException("Message data length is invalid");
 
         ProtocolVersion = data[2];
+
+        User = Encoding.UTF8.GetString(data, 3, 64);
+        Password = Encoding.UTF8.GetString(data, 67, 64);
+        Hostname = Encoding.UTF8.GetString(data, 131, 64);
+        Platform = Encoding.UTF8.GetString(data, 195, 8);
     }
 
     public byte[] encode()
     {
-        byte[] msg = new byte[4 + 3];
+        byte[] msg = new byte[4 + 2 + 1 + 64 + 64 + 64 + 8];
 
-        Buffer.BlockCopy(ProtocolMessageFactory.Converter.GetBytes((UInt32) 3), 0, msg, 0, 4);
+        Buffer.BlockCopy(ProtocolMessageFactory.Converter.GetBytes((UInt32) msg.Length - 4), 0, msg, 0, 4);
         Buffer.BlockCopy(ProtocolMessageFactory.Converter.GetBytes((UInt16) MessageType.Introduction), 0, msg, 4, 2);
         msg[6] = ProtocolVersion;
+
+        Buffer.BlockCopy(User.PaddedByteArray(64), 0, msg, 7, 64);
+        Buffer.BlockCopy(Password.PaddedByteArray(64), 0, msg, 71, 64);
+        Buffer.BlockCopy(Hostname.PaddedByteArray(64), 0, msg, 135, 64);
+        Buffer.BlockCopy(Platform.PaddedByteArray(8), 0, msg, 199, 8);
 
         return msg;
     }
 
     public override string ToString()
     {
-        return String.Format("<Introduction version={0}>", ProtocolVersion);
+        return String.Format("<Introduction user={0} host={1} platform={2} version={3}>",
+            User, Hostname, Platform, ProtocolVersion);
     }
 }
