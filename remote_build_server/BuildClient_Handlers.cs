05b8626679b28a8489765cedad350e4b8502cf3e
Terence Martin <github@nurdz.com>
2018-12-18 21:59:47 -0800

    Introduction proper Introduction handling
    To get started with our communications protocol, we now properly handle
    the Introduction message by making sure that it's the first message
    received, only one is ever sent, and that it's protocol version is the
    correct one.

--- remote_build_server/BuildClient_Handlers.cs
+++ remote_build_server/BuildClient_Handlers.cs
@@ -3,9 +3,42 @@
 // The state object for reading client data.
 public partial class BuildClient
 {
+    // Has the client transmitted an introduction message to us yet?
+    // We require one or anything that the client does is otherwise considered
+    // to be an error.
+    private bool hasIntroduced = false;
+
+    // Transmit an error message to the user and then optionally close the
+    // connection; the connection will be closed once we finish transmitting
+    // this error.
+    void SendError(UInt32 code, string errorMsg, bool closeAfter=true)
+    {
+        var error = new ErrorMessage(code, errorMsg);
+        error.CloseAfterSending = closeAfter;
+        Send(error);
+    }
+
+    void SendMessage(string msg)
+    {
+        Send(new MessageMessage(msg));
+    }
+
     void HandleIntroduction(IntroductionMessage message)
     {
-        Send(message);
+        if (hasIntroduced)
+        {
+            SendError(1000, "Introduction message has already been received");
+            return;
+        }
+
+        if (message.ProtocolVersion != 1)
+        {
+            SendError(1001, "Invalid protocol; only version 1 is supported");
+            return;
+        }
+
+        hasIntroduced = true;
+        SendMessage(String.Format("Hello, {0}", message.User));
     }
 
     void HandleMessage(MessageMessage message)
@@ -20,6 +53,15 @@ public partial class BuildClient
 
     private void Dispatch(IProtocolMessage message)
     {
+        // If we have not been introduced to the other end of the connection yet
+        // then trigger an error unless this message is the introduction message
+        // itself.
+        if (hasIntroduced == false && message.MsgID != MessageType.Introduction)
+        {
+            SendError(1002, "Invalid communications; no Introduction received");
+            return;
+        }
+
         switch (message.MsgID)
         {
             case MessageType.Introduction:
