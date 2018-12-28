e58df9be89d7696d2b3c4448cf8d3753fc52c1d2
Terence Martin <github@nurdz.com>
2018-12-27 20:50:48 -0800

    Clarify dispatching a little bit
    Using the recently added handlers, this clarifies the handling a little
    bit by ensuring that we issue protocol violations in the appropriate
    places.
    
    We also redact the client's ability to transmit textual and error
    messages because those are messages meant for us to convey information
    and not the other way around.

--- remote_build_server/BuildClient_Handlers.cs
+++ remote_build_server/BuildClient_Handlers.cs
@@ -58,6 +58,11 @@ public partial class BuildClient
         Send(message);
     }
 
+    /// <summary>
+    /// Invoked every time a message is received from the client. This does all
+    /// of the protocol handling work on our end, handling messages as
+    /// appropriate.
+    /// </summary>
     private void Dispatch(IProtocolMessage message)
     {
         // If we have not been introduced to the other end of the connection yet
@@ -65,24 +70,30 @@ public partial class BuildClient
         // itself.
         if (hasIntroduced == false && message.MsgID != MessageType.Introduction)
         {
-            SendError(1002, "Invalid communications; no Introduction received");
+            ProtocolViolationMessage(message, "First message must be an introduction");
             return;
         }
 
         switch (message.MsgID)
         {
+            // These messages are onlu valid when transmitted from the server to
+            // the client; if the client sends them to us, issue a protocol
+            // violation.
+            case MessageType.Message:
+            case MessageType.Error:
+                ProtocolViolationMessage(message, "These messages are for server use only");
+                break;
+
+            // This should always be the first message received on a connection
+            // (and is only valid as the first message). It has specific
+            // handling associated with it.
             case MessageType.Introduction:
                 HandleIntroduction(message as IntroductionMessage);
                 break;
 
-            case MessageType.Message:
-                HandleMessage(message as MessageMessage);
-                break;
-
-            case MessageType.Error:
-                HandleError(message as ErrorMessage);
-                break;
-
+            // The client is indicating that it's time to set up a new build.
+            // This message tells us what paths are being built so that we can
+            // set things up on our end.
             case MessageType.SetBuild:
                 HandleSetBuild(message as SetBuildMessage);
                 break;
