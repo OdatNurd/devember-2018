ae6f4503db8095a08fa10ac3146a17374aee3fbf
Terence Martin <github@nurdz.com>
2018-12-27 20:47:07 -0800

    Include some extra processing functions
    This includes some extra handling functions for messages and better
    documents the ones that exist.
    
    Newly added are the ability to echo a message back to the other end
    (for testing purposes) and the ability to generate a protocol violation
    message along with a reason why. This is used as an indication that the
    client has done something wrong, stating what the unexpected message was
    and why it was rejected.

--- remote_build_server/BuildClient_Handlers.cs
+++ remote_build_server/BuildClient_Handlers.cs
@@ -3,14 +3,19 @@ using System;
 // The state object for reading client data.
 public partial class BuildClient
 {
-    // Has the client transmitted an introduction message to us yet?
-    // We require one or anything that the client does is otherwise considered
-    // to be an error.
+    /// <summary>
+    /// Used to indicate whether the other side has transmitted an introduction
+    /// message yet.
+    /// </summary>
+    /// An introduction message must be the first message transmitted upon a new
+    /// incoming connection, which is used by the client to tell us who they are
+    /// as well as their credentials.
     private bool hasIntroduced = false;
 
-    // Transmit an error message to the user and then optionally close the
-    // connection; the connection will be closed once we finish transmitting
-    // this error.
+    /// <summary>
+    /// Transmit an error message to the user, optionally closing the connection
+    /// once the message has been transmitted.
+    /// </summary>
     void SendError(UInt32 code, string errorMsg, bool closeAfter=true)
     {
         var error = new ErrorMessage(code, errorMsg);
@@ -18,9 +23,39 @@ public partial class BuildClient
         Send(error);
     }
 
+    /// <summary>
+    /// Transmit an informational or warning message to the user; this is used
+    /// for all textual communications which are not considered an out and out
+    /// error.
+    /// </summary>
     void SendMessage(string msg)
     {
         Send(new MessageMessage(msg));
+    }
+
+    /// <summary>
+    /// Handle an incoming protocol message by sending back an error message
+    /// indicating that a message of this type is not allowed at this point in
+    /// the conversation.
+    /// </summary>
+    void ProtocolViolationMessage(IProtocolMessage message, string reason=null)
+    {
+        SendError(9999, String.Format(
+            "Received unexpected message with id={0}; protocol violation",
+            message.MsgID), reason == null);
+
+        if (reason != null)
+            SendError(9999, reason);
+    }
+
+    /// <summary>
+    /// Handle an incoming protocol message by echoing it back to the remote
+    /// client exactly as received. This is a useful test that both ends can
+    /// successfully encode and decode the message.
+    /// </summary>
+    void EchoMessage(IProtocolMessage message)
+    {
+        Send(message);
     }
 
     private void Dispatch(IProtocolMessage message)
