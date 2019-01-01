c84559ccd2117487d047263ea330961f11c8220b
Terence Martin <github@nurdz.com>
2018-12-31 20:46:52 -0800

    Include a simple program for remote build testing
    This adds a simple dotnet core console program that outputs a line of
    text to stdout and stderr (one each) for us to do our remote build
    testing with.

---
+++ test_project/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace test_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I'm simple test output");
+            Console.Error.WriteLine("I'm the error output");
+        }
+    }
+}
